065288dba90a279fa9c35eb41c46f18755968423
lracca1 <lracca@cnm.edu>
2018-06-29 10:44:39 -0600

    Initial commit

---
+++ src/main/java/cnm/edu/deepdive/emojipetsservice/EmojiPetsServiceApplication.java
@@ -0,0 +1,12 @@
+package cnm.edu.deepdive.emojipetsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiPetsServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmojiPetsServiceApplication.class, args);
+  }
+}
