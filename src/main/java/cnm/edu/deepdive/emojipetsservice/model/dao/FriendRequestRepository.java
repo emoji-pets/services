bd1b82499d51fa5ff7cbbbc456e6e87623d3b253
lracca1 <lracca@cnm.edu>
2018-07-24 10:57:21 -0600

    Updated ERD, and changed FriendRequestRepository class into an interface and added code.

--- src/main/java/cnm/edu/deepdive/emojipetsservice/model/dao/FriendRequestRepository.java
+++ src/main/java/cnm/edu/deepdive/emojipetsservice/model/dao/FriendRequestRepository.java
@@ -1,5 +1,8 @@
 package cnm.edu.deepdive.emojipetsservice.model.dao;
 
-public class FriendRequestRepository {
+import cnm.edu.deepdive.emojipetsservice.model.entity.Player;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FriendRequestRepository extends CrudRepository<Player, Long> {
 
 }
